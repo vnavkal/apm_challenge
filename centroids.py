c6d09cf67a367e1cf8fd85b71bdb2de6945bd6b6
Viraj Navkal <vnavkal0@gmail.com>
2016-07-23 14:35:39 -0700

    create and use _nth_proximities function

--- centroids.py
+++ centroids.py
@@ -1,48 +1,52 @@
-"""Calculates proximities between centroids and coordinate locations"""
+"""Calculates proximities between centroids and coordinate locations
 
-import numpy as np
+Public classes:
+    ClosestCentroidCalculator
+
+Public methods:
+    furthest_nth_coordinate
+"""
+
 from numbers import Number
+from typing import Iterable
 import heapq
+import numpy as np
 
-def distances(X: np.ndarray, y: np.ndarray):
+
+def furthest_nth_proximity(centroids: np.ndarray, coordinates: Iterable, n: int) -> np.float64:
+    """Max of the nth largest distance from a centroid to any coordinate"""
+    proximities = _nth_proximities(coordinates, centroids, n)
+    return proximities.max()
+
+
+class ClosestCentroidCalculator:
+    def __init__(self, centroids: np.ndarray, coordinates: Iterable) -> None:
+        self._proximities = _nth_proximities(centroids, coordinates, 1)
+
+    def num_coordinates_within(self, radius: Number) -> np.float64:
+        """Number of coordinates within specified radius of any centroid"""
+        return (self._proximities <= radius).sum()
+
+    def min_radius_enveloping_percent(self, percent: Number) -> np.float64:
+        """Smallest radius such that percent of coordinates are within radius of a centroid"""
+        return np.percentile(self._proximities, percent, interpolation='lower')
+
+
+def _nth_proximities(X: np.ndarray, Y: np.ndarray, n: int) -> np.ndarray:
+    """Calculate the nth smallest distance from each element of Y to any element of X"""
+    proximities = np.empty(len(Y))
+    for i, y in enumerate(Y):
+        proximities[i] = heapq.nsmallest(n, _distances(X, y))[-1]
+    return proximities
+
+
+def _distances(X: np.ndarray, y: np.ndarray) -> np.ndarray:
     _check_dimensions(X, y)
     return np.linalg.norm(X - y, axis=1)
 
 
-def within_distance(X, y, distance):
-    first_small_enough_distance = next(x for x in distances(X, y) if x <= distance)
-    return (first_small_enough_distance is not None)
-
-
-def _check_dimensions(X, y):
+def _check_dimensions(X, y) -> None:
     """Check X has dimension (n, 2) and y has dimension (n,)"""
     if X.shape != (y.shape[0], 2) or len(y.shape) > 1:
         raise ValueError('X and y have incorrect dimensions {0} and {1}'.
                          format(X.shape, y.shape))
-
-class ClosestCentroidCalculator:
-    def __init__(self, centroids: np.ndarray, coordinates: np.ndarray):
-        self._calculate_closest_centroids(self, centroids, coordinates)
-
-    def num_coordinates_within(self, radius: Number):
-        return (self._proximities <= radius).sum()
-
-    def min_radius_enveloping_percent(self, percent: Number):
-        return (np.percentile(self._proximities, percent, interpolation='lower'))
-
-    def _calculate_closest_centroids(self, centroids: np.ndarray, coordinates: np.ndarray):
-        proximities = np.empty(len(coordinates))
-        for i, coordinate_pair in enumerate(coordinates):
-            proximities[i] = 
-        self._proximities = np.array([
-            distances(centroids, coordinate_pair) for coordinate_pair in coordinates
-        ])
-
-class DistanceCalculator:
-    def __init__(self, X: np.ndarray, Y: np.ndarray, n: int):
-        self._calculate_nth_proximities(X, Y, n)
-
-    def _calculate_nth_proximities(X, Y, n):
-        self._proximities = np.empty(len(X))
-        for i, y in enumerate(Y):
-            self._proximities[i] = heapq.nsmallest(n, distances(X, y))[-1]
--- test_centroids.py
+++ test_centroids.py
@@ -1,16 +1,39 @@
 import unittest
 import numpy as np
-from centroids import distances_from_centroid
+from centroids import ClosestCentroidCalculator, furthest_nth_proximity
 
-class TestCentroids(unittest.TestCase):
-    def test_distances_from_centroids(self):
-        centroid = np.array((3, 4))
-        coordinates_collection = np.array(((3, 4),
-                                           (0, 0)))
-        distances = distances_from_centroid(centroid, coordinates_collection)
-        print('distances are {0}'.format(distances))
-        self.assertAlmostEqual(distances[0], 0)
-        self.assertAlmostEqual(distances[1], 5)
+
+class TestFurthestNthCoordinate(unittest.TestCase):
+    def test_furthest_nth_coordinate(self):
+        centroids = np.array(((0, 0),
+                              (1, 0)))
+        coordinates = np.array(((1, 1),
+                                (4, 4)))
+        self.assertAlmostEqual(furthest_nth_proximity(centroids, coordinates, 1),
+                               np.linalg.norm((1, 1)))
+        self.assertAlmostEqual(furthest_nth_proximity(centroids, coordinates, 2),
+                               np.linalg.norm((4, 4)))
+
+
+class TestClosestCentroidCalculator(unittest.TestCase):
+    def setUp(self):
+        centroids = np.array(((0, 0),
+                              (1, 0)))
+        coordinates = np.array(((1, 1),
+                                (4, 4)))
+        self._calculator = ClosestCentroidCalculator(centroids, coordinates)
+
+    def test_num_coordinates_within(self):
+        self.assertEqual(self._calculator.num_coordinates_within(0), 0)
+        self.assertEqual(self._calculator.num_coordinates_within(1), 1)
+        self.assertEqual(self._calculator.num_coordinates_within(5), 2)
+        self.assertEqual(self._calculator.num_coordinates_within(6), 2)
+
+    def test_min_radius_enveloping_percent(self):
+        # self.assertIsNone(self._calculator.min_radius_enveloping_percent(10))
+        self.assertAlmostEqual(self._calculator.min_radius_enveloping_percent(50), 1)
+        self.assertAlmostEqual(self._calculator.min_radius_enveloping_percent(100), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
